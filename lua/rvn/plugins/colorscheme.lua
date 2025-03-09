c47f3aebc10fe3c7eb36d429ed3dc0c95cd89b0e
probablyalright <112282114+probablyalright@users.noreply.github.com>
2025-03-09 21:33:08 +0200

    Add catppuccin colorscheme without italics

--- lua/rvn/plugins/colorscheme.lua
+++ lua/rvn/plugins/colorscheme.lua
@@ -1,13 +1,38 @@
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	config = function()
-		require('rose-pine').setup({
-			styles = {
-				italic = false,
-			},
-		})
+    {
+        "catppuccin/nvim",
+        config = function()
+            require("catppuccin").setup({
+                no_italic = true,
+                styles = {
+                    comments = { "italic" },
+                    conditionals = {},
+                    loops = {},
+                    functions = {},
+                    keywords = {},
+                    strings = {},
+                    variables = {},
+                    numbers = {},
+                    booleans = {},
+                    properties = {},
+                    types = {},
+                    operators = {},
+                },
+            })
 
-		vim.cmd("colorscheme rose-pine")
-	end
+            vim.cmd.colorscheme "catppuccin"
+        end,
+
+    },
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        config = function()
+            require("rose-pine").setup({
+                styles = {
+                    italic = false,
+                },
+            })
+        end
+    },
 }
